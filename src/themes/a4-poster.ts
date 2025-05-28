9601e5528386bf890424e0c41a232861e7e7af74
Cian O Mahony <ciaantonnta@gmail.com>
2025-05-28 17:45:36 +0100

    consistency & margins

--- src/themes/a4-poster.ts
+++ src/themes/a4-poster.ts
@@ -2,6 +2,10 @@ import { Theme } from "../types";
 
 export const a4PosterTheme: Theme = {
   name: "a4-poster",
+  fontLinks: [
+    "https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap",
+    "https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,600;1,400;1,600&display=swap",
+  ],
   wrapperStyle: {
     width: "794px",
     height: "1123px",
@@ -9,7 +13,7 @@ export const a4PosterTheme: Theme = {
     fontFamily: '"Inter", sans-serif',
     color: "#000",
     textAlign: "left",
-    padding: "80px",
+    padding: "96px",
     boxSizing: "border-box",
     display: "flex",
     flexDirection: "column",
@@ -18,43 +22,45 @@ export const a4PosterTheme: Theme = {
   elementStyles: {
     h1: {
       fontFamily: '"Playfair Display", serif',
-      fontSize: "52px",
+      fontSize: "48px",
       fontWeight: "600",
-      margin: "0 0 8px 0",
+      margin: "0 0 24px 0",
       width: "100%",
       maxWidth: "650px",
     },
     h2: {
       fontFamily: '"Playfair Display", serif',
-      fontSize: "52px",
-      fontStyle: "italic",
+      fontSize: "36px",
       fontWeight: "600",
-      margin: "0 0 40px 0",
+      margin: "0 0 20px 0",
       width: "100%",
       maxWidth: "650px",
     },
     h3: {
       fontFamily: '"Playfair Display", serif',
-      fontSize: "36px",
+      fontSize: "28px",
       fontWeight: "600",
-      margin: "1em 0 0.5em",
+      margin: "0 0 16px 0",
       width: "100%",
       maxWidth: "650px",
     },
+
     p: {
-      fontSize: "20px",
+      fontSize: "18px",
       lineHeight: "1.75",
-      marginBottom: "24px",
+      margin: "0 0 16px 0",
       width: "100%",
       maxWidth: "650px",
     },
+
     em: { fontStyle: "italic" },
     strong: { fontWeight: "bold" },
     u: { textDecoration: "underline" },
     s: { textDecoration: "line-through" },
+
     blockquote: {
-      marginLeft: "2rem",
-      paddingLeft: "1.5rem",
+      margin: "0 0 16px 40px",
+      paddingLeft: "20px",
       borderLeft: "3px solid #000",
       fontStyle: "italic",
       color: "#333",
@@ -73,31 +79,41 @@ export const a4PosterTheme: Theme = {
       padding: "1.5em",
       borderRadius: "6px",
       overflowX: "auto",
+      margin: "0 0 24px 0",
       width: "100%",
       maxWidth: "650px",
     },
     ul: {
-      marginLeft: "1.5rem",
-      paddingLeft: "1rem",
+      margin: "0 0 16px 0",
+      paddingLeft: "40px",
+      fontSize: "18px",
       width: "100%",
       maxWidth: "650px",
+      listStyleType: "disc",
     },
     ol: {
-      marginLeft: "1.5rem",
-      paddingLeft: "1rem",
+      margin: "0 0 16px 0",
+      paddingLeft: "40px",
+      fontSize: "18px",
       width: "100%",
       maxWidth: "650px",
+      listStyleType: "decimal",
     },
-    li: { marginBottom: "0.5em", fontSize: "20px" },
+    li: {
+      marginBottom: "8px",
+      fontSize: "18px",
+    },
+
     a: { color: "#2b6cb0", textDecoration: "underline" },
+
     div: {
       fontFamily: '"Playfair Display", serif',
-      fontSize: "20px",
+      fontSize: "18px",
       fontWeight: "bold",
       letterSpacing: "2px",
-      marginTop: "40px",
+      marginTop: "32px",
       width: "100%",
       maxWidth: "650px",
     },
   },
-}; 
+};
