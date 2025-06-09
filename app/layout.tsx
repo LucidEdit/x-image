80f66e593dcb285692129fba2ea031aa20a55aee
Ray Fitzgerald <56260742+TheRayFitzgerald@users.noreply.github.com>
2025-06-09 22:24:55 +0100

    OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,40 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const APP_URL = "https://x.lucid.so";
+const APP_NAME = "Lucid";
+const APP_DEFAULT_TITLE = "Lucid - Beautiful Text Images";
+const APP_TITLE_TEMPLATE = "%s - Lucid";
+const APP_DESCRIPTION = "Lucid - Beautiful Text Images";
+
 export const metadata: Metadata = {
   title: "Lucid - Beautiful Text Images",
-  description: "Lucid - Writing Reimagined",
+  description: "Create beautiful text images with Lucid",
+  openGraph: {
+    type: "website",
+    url: APP_URL,
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    images: [
+      {
+        url: `${APP_URL}/og-image.png`,
+        width: 1200,
+        height: 630,
+        alt: APP_NAME,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: APP_DEFAULT_TITLE,
+    description: APP_DESCRIPTION,
+    creator: "@rincidium",
+    images: `${APP_URL}/og-image.png`,
+  },
 };
 
 export default function RootLayout({
