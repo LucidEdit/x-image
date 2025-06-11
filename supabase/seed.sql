4e1e1666d13b869cb007ec683aa0eeb01e1035fa
Ray Fitzgerald <56260742+TheRayFitzgerald@users.noreply.github.com>
2025-06-11 10:01:24 +0100

    update RLS

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,8 +1,16 @@
 -- Note: RLS is already enabled by default on storage.objects in Supabase
--- Very open policy for x-images bucket (public community feed)
-CREATE POLICY "Allow all operations on x-images bucket"
+-- Secure policies for x-images bucket (public community feed)
+
+-- Allow public read access to PNG files in x-images bucket
+CREATE POLICY "Allow SELECT on x-images PNG files"
 ON storage.objects
-FOR ALL
+FOR SELECT
 TO public
-USING (bucket_id = 'x-images')
-WITH CHECK (bucket_id = 'x-images'); 
+USING ((bucket_id = 'x-images'::text) AND (storage.extension(name) = 'png'::text));
+
+-- Allow public upload of PNG files to x-images bucket
+CREATE POLICY "Allow INSERT on x-images PNG files"
+ON storage.objects
+FOR INSERT
+TO public
+WITH CHECK ((bucket_id = 'x-images'::text) AND (storage.extension(name) = 'png'::text)); 
