1f2cb092ed6d7b4b07996b620a8a3ab03af14499
Cian O Mahony <ciaantonnta@gmail.com>
2025-06-23 16:22:16 +0100

    new beige green theme

--- text-to-image/themes/index.ts
+++ text-to-image/themes/index.ts
@@ -3,6 +3,7 @@ import { getThemes, getThemeByName, getAvailableThemeNames } from "./registry";
 import "./book-excerpt";
 import "./dark-mono-poster";
 import "./manifesto";
+import "./beige-green";
 import "./forest";
 
 export const themes = getThemes();
